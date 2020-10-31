c1ed258e6021863a19800844c10ee579f5113441
Lukaribou <lucasperroud17@gmail.com>
2020-10-31 11:41:50 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{
+    fs,
+    env,
+};
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string(get_file_to_open())
+        .expect("Error while reading the file.");
+
+    println!("{}", content);
+}
+
+fn get_file_to_open() -> String {
+    return String::from("E:\\Projets\\Langages\\Rust\\BrainfuckInterpreter\\bf\\main.bf")
+    /*return if env::args().len() == 0 {
+        String::from(format!("../bf/{}", "main.bf"))
+    } else {
+        format!("../bf/{}", env::args().collect::<Vec<String>>()[0].to_owned())
+    }*/
 }
